72f94f739f84ae21fb28ebff5ac9e039fcd824c1
elim-minwoo <minwoo.han@s.elimmtalbert.school.nz>
2025-06-19 02:08:03 +1200

    slider index counter

--- slider.js
+++ slider.js
@@ -10,16 +10,28 @@ appendChild = add child element to the end of another child element
 */
 
 function SliderScript() {
-  const slides = document.getElementById('slides'); // variable "slides" will always refer to elements with the id "slides"
-  const slideWidth = 600;
-  let index = 1; // count index
-  let isMoving = false; // prevent overlapping animation
+    const slides = document.getElementById('slides'); // variable "slides" will always refer to elements with the id "slides"
+    const slideWidth = 600;
+    let index = 1; // count index
+    let isMoving = false; // prevent overlapping animation
 
-  const imgArray = Array.from(slides.children); // convert everything to array
-  const firstClone = imgArray[0].cloneNode(true); // copy and store image
-  const lastClone = imgArray[imgArray.length - 1].cloneNode(true); // copy and store image
+    const imgArray = Array.from(slides.children); // convert everything to array
+    const firstClone = imgArray[0].cloneNode(true); // copy and store image
+    const lastClone = imgArray[imgArray.length - 1].cloneNode(true); // copy and store image
 
-  slides.appendChild(firstClone); // add first image clone to end
-  slides.insertBefore(lastClone, slides.firstChild); // add last image clone to front
-  slides.style.transform = `translateX(-${slideWidth * index}px)`;
+    slides.appendChild(firstClone); // add first image clone to end
+    slides.insertBefore(lastClone, slides.firstChild); // add last image clone to front
+    slides.style.transform = `translateX(-${slideWidth * index}px)`;
 }
+
+function moveSlide(toIndex) { // function to move slide to another
+    if (isMoving) return; // fix spamming click freezing slider
+    isMoving = true;
+    slides.style.transition = 'transform 0.5s ease'; // css transition animation
+    slides.style.transform = `translateX(-${slideWidth * toIndex}px)`; // move slider container horizontally
+    index = toIndex; // update slide index counter to new index
+  }
+
+document.getElementById('nextBtn').onclick = () => moveSlide(index + 1);
+document.getElementById('prevBtn').onclick = () => moveSlide(index - 1);
+
